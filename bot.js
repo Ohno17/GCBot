eafa83364f8e2345636189ad517dc6c31d985e0f
Ohno17 <ajaylego30@gmail.com>
2023-08-07 22:54:16 -0500

    Added polling functionality.

--- bot.js
+++ bot.js
@@ -3,17 +3,25 @@
 
 const MESSAGE_DELAY = 1000;
 const observer = new MutationObserver(onNewMessage);
+const advice = [
+	"Advice."
+];
 let HTMLPolicy;
 
 if (window.trustedTypes) {
 	HTMLPolicy = trustedTypes.createPolicy("HTMLPolicy", {
 		createHTML: (string) => string,
+		createScriptURL: (string) => string,
+    createScript: (string) => string,
 	});
 }
 
 var currentSender = null;
+var currentPoll = null;
+var eggCounter = 0;
 
 function findButtonAndTextBox() {
+
 	window.bot_sendbutton = document.getElementById("bot_sendbutton");
 	window.bot_textbox = document.getElementById("bot_textbox");
 	window.bot_messages = document.getElementById("bot_messages");
@@ -65,39 +73,138 @@ function isPositiveInteger(value) {
     return /^\d+$/.test(value);
 }
 
+function selectCommand(command) {
+	// command[0] is the command name, command [1 and beyond] is for parameters to that command
+
+	switch (command[0]) {
+		case "help":
+			sendMessage("Commands:\n/help\n/dice (max)\n/hello\n/timer (seconds)\n/advice\n/source\n/egg\n/myname\n/createpoll (name) (option1) (option 2) ...\n/pollhelp\n/pollvote (option number)");
+			break;
+		case "hello":
+			// Say hello
+			sendMessage("Hello!");
+			break;
+		case "dice":
+			// Roll a dice with specified max, otherwise max is 6.
+			var max;
+			if (command[1] && isPositiveInteger(command[1])) {
+				max = command[1];
+			} else {
+					max = 6;
+			}
+
+			const result = Math.round(Math.random() * (max - 1)) + 1;
+			sendMessage("You rolled a " + result + "!");
+			break;
+		case "timer":
+			// Set a timer in seconds.
+			if (command[1] && isPositiveInteger(command[1])) {
+				setTimeout(function () {
+					sendMessage("Time's up!");
+				}, command[1] * 1000);
+			} else {
+				sendMessage("I didn't understand that!");
+			}
+			
+			break;
+		case "advice":
+			const selected = advice[Math.round(Math.random() * (advice.length - 1))];
+			sendMessage(selected);
+			break;
+		case "source":
+			sendMessage("https://github.com/Ohno17/GCBot");
+			break;
+		case "egg":
+			eggCounter++;
+			sendMessage("Egg counter: " + eggCounter);
+			break;
+		case "myname":
+			sendMessage("Your name is: '" + currentSender + "'");
+			break;
+		case "createpoll":
+			if (currentPoll != null) {
+				sendMessage("There is already an active poll!");
+				return;
+			}
+
+			if (!command[1] || !command[2] || !command[3]) {
+				sendMessage("This command is malformed!");
+				return;
+			}
+
+			var polloptions = command.slice(2);
+			var preparedoptions = [];
+
+			for (var i = 0; i < polloptions.length; i++) {
+				preparedoptions.push([polloptions[i], 0]);
+			}
+
+			currentPoll = {title: command[1].replaceAll("+", " "), options: preparedoptions, voters: []}
+			sendMessage("Made a poll: '" + currentPoll.title + "'\nUse '/pollhelp' for information about this poll!");
+
+			break;
+		case "pollhelp":
+			if (currentPoll == null) {
+				sendMessage("There is no active poll!");
+				return;
+			}
+
+			var pollinfostring = "'" + currentPoll.title + "'\n\n";
+
+			for (var i = 0; i < currentPoll.options.length; i++) {
+				pollinfostring += "Option " + (i + 1) + ": '" + currentPoll.options[i][0] + "' (Vote for this using '/pollvote " + (i + 1) + "')\n"
+			}
+
+			sendMessage(pollinfostring);
+			break;
+		case "pollvote":
+			if (currentPoll == null) {
+				sendMessage("There is no active poll!");
+				return;
+			}
+
+			if (!command[1] || !isPositiveInteger(command[1])) {
+				sendMessage("Please vote properly!");
+				return;
+			}
+
+			if ((parseInt(command[1]) - 1) > (currentPoll.options.length - 1) || !(parseInt(command[1]) > 0)) {
+				sendMessage("This option doesn't exist!");
+				return;
+			}
+
+			currentPoll.options[parseInt(command[1]) - 1][1]++;
+			sendMessage("Voted for: '" + currentPoll.options[parseInt(command[1]) - 1][0] + "'");
+			break;
+		case "deletepoll":
+			if (currentPoll == null) {
+				sendMessage("There is no active poll!");
+			} else {
+				currentPoll = null;
+				sendMessage("Poll deleted!");
+			}
+			break;
+		default:
+			sendMessage("I didn't understand that!");
+			break;
+	}
+}
+
 function onNewMessage() {
 	// Run on new message
 	// Check for slash, indicating command to bot
 	const message = readMessage();
+	console.log("MESSAGE RECIVED", "SENDER = " + currentSender);
 	if (Array.from(message)[0] == "/") {
 		const command = message.substring(1).split(" ");
 
-		// command[0] is the command name, command [1 to infinity] is for parameters to that command
+		try {
+			selectCommand(command);
+		} catch(error) {
+			console.error(error);
+			stopBot();
+		}
 
-		switch (command[0]) {
-			case "help":
-				sendMessage("Commands: /help, /dice (max), /hello");
-				break;
-			case "hello":
-				// Say hello
-				sendMessage("Hello!");
-				break;
-			case "dice":
-				// Roll a dice with specified max, otherwise max is 6.
-				var max;
-				if (command[1] && isPositiveInteger(command[1])) {
-					max = command[1];
-				} else {
-					max = 6;
-				}
-
-				const result = Math.round(Math.random() * max);
-				sendMessage("You rolled a " + result + "!");
-				break;
-			default:
-				sendMessage("I didn't understand that!");
-				break;
-	 }
 	}
 }
 
@@ -107,10 +214,12 @@ function main() {
 	sendMessage("I am OkBot! Type /help for a list of commands.");
 }
 
-function startBot() {
+function startBot(silence) {
 	if (findButtonAndTextBox()) throw Error("Required elements not found.");
 	addMessageListener();
-	main();
+	if (!silence) {
+		main();
+	}
 }
 
 function stopBot() {
